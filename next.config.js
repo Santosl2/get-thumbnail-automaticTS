04f735782a0c0796c268a1986473d8d0d77faf24
Santosl2c <mfilype2019@gmail.com>
2022-01-18 08:58:41 -0300

    refactor: removed unused libs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-  webpack: function (config) {
-    config.module.rules.push({ test: /\.md$/, use: "raw-loader" });
 
-    return config;
-  },
   env: {
     BASE_URL: process.env.BASE_URL,
     SITE_NAME: process.env.SITE_NAME,
